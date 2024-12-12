2a5bac37f8aff0b97a920cb2ed2380c081984ef9
Mohamad Shahin <mohamad.shahin2002@gmail.com>
2024-12-12 16:00:08 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/local_naming_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.local_naming_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:nowa_runtime/nowa_runtime.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:local_naming_test/globals/app_state.dart';
+import 'package:local_naming_test/pages/home_page.dart';
+
+@NowaGenerated()
+late final SharedPreferences sharedPrefs;
+
+@NowaGenerated()
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  sharedPrefs = await SharedPreferences.getInstance();
+
+  runApp(const MyApp());
+}
+
+@NowaGenerated({'visibleInNowa': false})
+class MyApp extends StatelessWidget {
+  @NowaGenerated({'loader': 'auto-constructor'})
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<AppState>(
+      create: (context) => AppState(),
+      builder: (context, child) => MaterialApp(
+        theme: AppState.of(context).theme,
+        initialRoute: 'HomePage',
+        routes: {'HomePage': (context) => const HomePage()},
+      ),
+    );
+  }
+}
