cb0f4048e91fa7f80263fb8c6abafdb14c413c18
Mohamad Shahin <mohamad.shahin2002@gmail.com>
2024-12-17 12:08:15 +0300

    helloooo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Home Test3',
+          'Home Test5',
         ),
       ),
       body: const SafeArea(
