3abe438553a9216dd0cc3104f1f7aaafc3bc4c19
Mohamad Shahin <mohamad.shahin2002@gmail.com>
2024-12-17 16:30:35 +0300

    Test

---
+++ lib/basic_cards11.dart
@@ -0,0 +1,270 @@
+import 'package:flutter/material.dart';
+import 'package:nowa_runtime/nowa_runtime.dart';
+
+@NowaGenerated({'auto-width': 393.0, 'auto-height': 808.0})
+class BasicCards11 extends StatelessWidget {
+  @NowaGenerated({'loader': 'auto-constructor'})
+  const BasicCards11({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NFlex(
+        direction: Axis.vertical,
+        spacing: 0.0,
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          FlexSizedBox(
+            width: double.infinity,
+            height: 71.0,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+                top: 10.0,
+                bottom: 10.0,
+              ),
+              child: NFlex(
+                direction: Axis.horizontal,
+                spacing: 164.0,
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  FlexSizedBox(
+                    width: null,
+                    child: Text(
+                      'Welcome Back!',
+                      style: TextStyle(
+                        shadows: [],
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                  FlexSizedBox(
+                    width: 40.0,
+                    height: 40.0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xffc4c4c4),
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          FlexSizedBox(
+            width: 390.0,
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+                top: 10.0,
+                bottom: 10.0,
+              ),
+              child: NFlex(
+                direction: Axis.vertical,
+                spacing: 20.0,
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  FlexSizedBox(
+                    width: double.infinity,
+                    height: null,
+                    flex: 1,
+                    child: NFlex(
+                      direction: Axis.vertical,
+                      spacing: 8.0,
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        FlexSizedBox(
+                          width: double.infinity,
+                          flex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xffc4c4c4),
+                              borderRadius: BorderRadius.circular(20.0),
+                              boxShadow: [],
+                            ),
+                          ),
+                        ),
+                        FlexSizedBox(
+                          width: null,
+                          height: null,
+                          child: Text(
+                            'Cillum minim aliquip.',
+                            style: TextStyle(
+                              shadows: [],
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  FlexSizedBox(
+                    width: double.infinity,
+                    height: null,
+                    flex: 1,
+                    child: NFlex(
+                      direction: Axis.vertical,
+                      spacing: 8.0,
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        FlexSizedBox(
+                          width: double.infinity,
+                          flex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xffc4c4c4),
+                              borderRadius: BorderRadius.circular(20.0),
+                              boxShadow: [],
+                            ),
+                          ),
+                        ),
+                        FlexSizedBox(
+                          width: null,
+                          height: null,
+                          child: Text(
+                            'Cillum minim aliquip.',
+                            style: TextStyle(
+                              shadows: [],
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  FlexSizedBox(
+                    width: double.infinity,
+                    height: null,
+                    flex: 1,
+                    child: NFlex(
+                      direction: Axis.vertical,
+                      spacing: 8.0,
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        FlexSizedBox(
+                          width: double.infinity,
+                          flex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xffc4c4c4),
+                              borderRadius: BorderRadius.circular(20.0),
+                              boxShadow: [],
+                            ),
+                          ),
+                        ),
+                        FlexSizedBox(
+                          width: null,
+                          height: null,
+                          child: Text(
+                            'Cillum minim aliquip.',
+                            style: TextStyle(
+                              shadows: [],
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  FlexSizedBox(
+                    width: double.infinity,
+                    height: null,
+                    flex: 1,
+                    child: NFlex(
+                      direction: Axis.vertical,
+                      spacing: 8.0,
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        FlexSizedBox(
+                          width: double.infinity,
+                          flex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xffc4c4c4),
+                              borderRadius: BorderRadius.circular(20.0),
+                              boxShadow: [],
+                            ),
+                          ),
+                        ),
+                        FlexSizedBox(
+                          width: null,
+                          height: null,
+                          child: Text(
+                            'Cillum minim aliquip.',
+                            style: TextStyle(
+                              shadows: [],
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  FlexSizedBox(
+                    width: double.infinity,
+                    height: null,
+                    flex: 1,
+                    child: NFlex(
+                      direction: Axis.vertical,
+                      spacing: 8.0,
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        FlexSizedBox(
+                          width: double.infinity,
+                          flex: 1,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color(0xffc4c4c4),
+                              borderRadius: BorderRadius.circular(20.0),
+                              boxShadow: [],
+                            ),
+                          ),
+                        ),
+                        FlexSizedBox(
+                          width: null,
+                          height: null,
+                          child: Text(
+                            'Cillum minim aliquip.',
+                            style: TextStyle(
+                              shadows: [],
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
